8a57d551dc8d4457ba7f68c392471cacedcc32f0
harryxu <harryzhxu@gmail.com>
2011-01-23 02:03:19 +0800

    saving lat, lng and zoom

--- gmapfield.js
+++ gmapfield.js
@@ -10,21 +10,34 @@ Drupal.behaviors.gmapfield = {
 Drupal.gmapfield = {
     initMap: function(delta) {
         var latInput = $('#gmapfield-'+delta+'-lat');
-        var lngInput = $('#gmapfield-'+delta+'-long');
-        var latlng = new google.maps.LatLng(
-            Number(latInput.val()), Number(lngInput.val()));
+        var lngInput = $('#gmapfield-'+delta+'-lng');
+        var zoomInput = $('#gmapfield-'+delta+'-zoom');
+        var lat = Number(latInput.val());
+        var lng = Number(lngInput.val());
+        var latlng = new google.maps.LatLng(lat, lng);
         var options = {
-            zoom: 8,
+            zoom: Number(zoomInput.val()),
             center: latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var canvasId = "gmapfield-"+delta+"-canvas";
         var map = new google.maps.Map($('#'+canvasId).get(0), options);
-        google.maps.event.addListener(map, 'center_changed', function() {
-            var latlng = map.getCenter();
+        var marker = new google.maps.Marker({
+                position: latlng,
+                draggable: true,
+        });
+        marker.setMap(map);
+
+        var handler = function(event) {
+            var latlng = event.latLng;
+            marker.setPosition(latlng);
             latInput.val(latlng.lat());
             lngInput.val(latlng.lng());
-        });
+            zoomInput.val(map.getZoom());
+        };
+
+        google.maps.event.addListener(map, 'click', handler);
+        google.maps.event.addListener(marker, 'dragend', handler);
     }
 };
 
