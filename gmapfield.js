ef33e904cd67a7f82aa19e330d620845ae130c4c
harryxu <harryzhxu@gmail.com>
2011-01-23 14:02:21 +0800

    put mapfield under a fieldset

--- gmapfield.js
+++ gmapfield.js
@@ -2,43 +2,51 @@
 Drupal.behaviors.gmapfield = {
     attach: function(context, settings) {
         for (var i in settings.gmapfields) {
-            Drupal.gmapfield.initMap(settings.gmapfields[i]);
+            var delta = settings.gmapfields[i];
+            new Drupal.GMapField(delta);
         }
     },
 };
 
-Drupal.gmapfield = {
-    initMap: function(delta) {
-        var latInput = $('#gmapfield-'+delta+'-lat');
-        var lngInput = $('#gmapfield-'+delta+'-lng');
-        var zoomInput = $('#gmapfield-'+delta+'-zoom');
-        var lat = Number(latInput.val());
-        var lng = Number(lngInput.val());
-        var latlng = new google.maps.LatLng(lat, lng);
-        var options = {
-            zoom: Number(zoomInput.val()),
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var canvasId = "gmapfield-"+delta+"-canvas";
-        var map = new google.maps.Map($('#'+canvasId).get(0), options);
-        var marker = new google.maps.Marker({
-                position: latlng,
-                draggable: true,
-        });
-        marker.setMap(map);
+Drupal.GMapField = function(delta) {
+    var self = this;
+    this.fieldset = $('#gmapfield-edit-'+delta);
+    this.fieldset.find('.fieldset-title').click(function(event){
+        self.fieldset.unbind(event);
+        self.initMap();
+    });
+}
 
-        var handler = function(event) {
-            var latlng = event.latLng;
-            marker.setPosition(latlng);
-            latInput.val(latlng.lat());
-            lngInput.val(latlng.lng());
-            zoomInput.val(map.getZoom());
-        };
+Drupal.GMapField.prototype.initMap = function() {
+    var latInput = this.fieldset.find('.gmapfield-lat');
+    var lngInput = this.fieldset.find('.gmapfield-lng');
+    var zoomInput = this.fieldset.find('.gmapfield-zoom');
+    var canvas = this.fieldset.find('.gmapfield-map-canvas').width(500).height(400).get(0);
+    var lat = Number(latInput.val());
+    var lng = Number(lngInput.val());
+    var latlng = new google.maps.LatLng(lat, lng);
+    var options = {
+        zoom: Number(zoomInput.val()),
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(canvas, options);
+    var marker = new google.maps.Marker({
+            position: latlng,
+            draggable: true,
+    });
+    marker.setMap(map);
 
-        google.maps.event.addListener(map, 'click', handler);
-        google.maps.event.addListener(marker, 'dragend', handler);
-    }
-};
+    var handler = function(event) {
+        var latlng = event.latLng;
+        marker.setPosition(latlng);
+        latInput.val(latlng.lat());
+        lngInput.val(latlng.lng());
+        zoomInput.val(map.getZoom());
+    };
+
+    google.maps.event.addListener(map, 'click', handler);
+    google.maps.event.addListener(marker, 'dragend', handler);
+}
 
 })(jQuery);
